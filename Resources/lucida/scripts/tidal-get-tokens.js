4267383c2e74892317f3ee67241b38da79eaa19e
patronsaynt <188747823+patronsaynt@users.noreply.github.com>
2026-05-04 13:43:15 -0400

    Remove unused files/folders

--- Resources/lucida/scripts/tidal-get-tokens.js
+++
@@ -1,7 +0,0 @@
-import Tidal from './../build/streamers/tidal/main.js'
-
-const client = new Tidal({
-	tvToken: process.env.TV_TOKEN,
-	tvSecret: process.env.TV_SECRET
-})
-client.getTokens()
--- Resources/lucida/src/streamers/tidal/constants.ts
+++
@@ -1,3 +0,0 @@
-export const TIDAL_AUTH_BASE = 'https://auth.tidal.com/v1/'
-export const TIDAL_API_BASE = 'https://listen.tidal.com/v1/'
-export const TIDAL_SUBSCRIPTION_BASE = 'https://api.tidal.com/v1/'
